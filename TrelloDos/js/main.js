dab1aa6334a3ee57490438b12c03870bfb795aab
IsoldaCordova <Isolda.Cordova@gmail.com>
2016-10-31 11:18:12 -0600

    trello 2

---
+++ TrelloDos/js/main.js
@@ -0,0 +1,83 @@
+var listaUno = document.getElementById("boton");
+listaUno.addEventListener("click", añadirLista);
+var contenedor = document.getElementById("lista");
+var contadorId = 1;
+	
+
+
+// da click al primer boton y sale un input para agregar texto y un boton
+function añadirLista(){
+	// crear listas
+
+	var listaName = document.createElement("input");
+		listaName.setAttribute("placeholder","añadir titulo");
+	var boton = document.createElement("button");
+		boton.setAttribute("type","button");
+		boton.textContent = ("Guardar");
+	contenedor.appendChild(listaName);
+	contenedor.appendChild(boton);
+ 	// Dar click en boton guardar y se borra el boton
+ 	boton.addEventListener("click",crearTitulo);
+
+ 	boton.onclick = function(){
+ 		boton.parentElement.removeChild(listaName);
+ 		boton.parentElement.removeChild(boton);
+ 		}
+ // da click en boton2 y abre cuadro con titulo, un textarea para agregar listas y el boton para guardarlos
+ 	function crearTitulo(){
+ 		// crea titulo y arroja boton con textarea para crear los titulos de las tareas
+ 		var cuadritoUno = document.createElement("div");
+			cuadritoUno.setAttribute("class", "jumbotron");
+
+
+ 		var tituloName = document.getElementById("tema");
+ 		var titulo =  document.createElement("h2");
+ 			titulo.innerHTML = listaName.value;
+ 		var texto = document.createElement("textarea");
+			texto.innerHTML = listaName.value;
+			texto.value = ("placeholder","añadir lista");
+ 		var botonDos = document.createElement("button");
+ 			botonDos.setAttribute("type","button");
+ 			botonDos.style.display="block";
+			botonDos.textContent = ("Guardar lista");
+		tituloName.appendChild(cuadritoUno);
+		cuadritoUno.appendChild(titulo);
+		cuadritoUno.appendChild(texto);
+		cuadritoUno.appendChild(botonDos);
+		cuadritoUno.addEventListener("drop",soltar);// darle atributo de soltar a mi caja principal que abraza las listas
+		cuadritoUno.addEventListener("dragover", arrastrarSobre);// atributo arrastrar sobre
+		cuadritoUno.addEventListener("dragleave", dejarArrastrar);// atributo dejar-arrastrar
+		
+		botonDos.addEventListener("click",listas);// dar click y se hace la funcion Listas
+
+
+		function listas(){
+			//crea los titulos de tareas
+			var p = document.createElement("p");
+				p.innerHTML = texto.value;
+				cuadritoUno.appendChild(p)
+				p.draggable = "true";
+				p.setAttribute("id","idTarjetitas"+ contadorId);
+				p.addEventListener("dragstart", arrastrar);// inicio para arrastrar mi tarjeta
+				p.addEventListener("dragend", terminarArrastrar);// terminar de arrastrar mi tarjeta
+				contadorId++;	
+			}
+ 	}
+ 	function arrastrar(evt){
+ 		evt.dataTransfer.setData("text",this.id);// funcion de p
+ 	}
+ 	function arrastrarSobre(evt){
+ 		evt.preventDefault();// dice que haga las funciones establecidas y no actue por accion default
+ 	}
+ 	function soltar(evt){
+ 		var idTransfer = evt.dataTransfer.getData("text");	
+ 		var pMove = document.getElementById(idTransfer);
+ 		this.insertBefore(pMove, this.childNodes[1]);
+ 	}
+ 	function terminarArrastrar(evt){
+
+ 	}
+ 	function dejarArrastrar(evt){
+
+ 	}
+ }
